4e5a3db6bfa14da9b7ad15719fe8d602b6485ebf
AWP22nd <aaawwwppp22@gmail.com>
2026-07-07 20:53:29 +0700

    Set up HTTP server on localhost:3002

--- cuy-university/web-sesi7/index.js
+++ cuy-university/web-sesi7/index.js
@@ -1,3 +1,12 @@
 const http = require('http');
+const host = '127.0.0.1';
+const port = 3002;
 
-console.log(http);
+// In n Out
+const server = http.createServer (function (request, response) {
+    response.end('Hello World')
+});
+
+server.listen(port, host, function () {
+    console.log(`Server On . . . ${host}: ${port}`);
+});
