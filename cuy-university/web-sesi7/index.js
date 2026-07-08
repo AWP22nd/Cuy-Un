f4f4411487509391d843b195ad4ab97d6d0c0d76
AWP22nd <aaawwwppp22@gmail.com>
2026-07-08 20:21:14 +0700

    Implement shopping summary in HTTP response
    Added functionality to display shopping details and remaining money in Rupiah format.

--- cuy-university/web-sesi7/index.js
+++ cuy-university/web-sesi7/index.js
@@ -1,10 +1,32 @@
 const http = require('http');
+const rupiah = require('rupiah-format');
 const host = '127.0.0.1';
 const port = 3002;
 
 // In n Out
 const server = http.createServer (function (request, response) {
-    response.end('Hello World')
+    const name = 'Skebow';
+    const mon = 1000000;
+    const shopping = 100000;
+    const chan = mon - shopping;
+
+    const sisaRupiah = rupiah.convert(sisa);
+    console.log('Sisa Rupiah: ', sisaRupiah);
+
+    const resl = `
+    
+    <head>
+        <title>${name}</title>
+    </head>
+    <body>
+        <h1 style = 'background: green; color: white; padding: 18px; text-align: center'> NODE JS </h1>
+        <p>
+            Hi ${name}! Anda belanja sebanyak ${shopping}, uang yang Anda gunakan sebnayak ${mon} lalu menjadi ${chan}.
+        </p>
+    </body>    
+    `
+    response.statusCode = 200;
+    response.end(resl)
 });
 
 server.listen(port, host, function () {
