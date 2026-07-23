ce0cab7c7a14a09910a392092af1e7e632e350a9
AWP22nd <aaawwwppp22@gmail.com>
2026-07-23 19:16:08 +0700

    Update index.js

---
+++ cuy-university/sesi8/index.js
@@ -0,0 +1,14 @@
+let passwordLength = document.getElementById("passwordLength")
+let password = document.getElementById("password")
+
+function getPassword() {
+    const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz"
+    const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const numeric = "0123456789"
+    const symbol = "!@#$%^&*()_+=-{}[]';://.,<>`~"
+    
+}
+
+function savePassword() {
+
+}
