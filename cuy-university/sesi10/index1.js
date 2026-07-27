f7597c93c3ed992ccf41f339e508e426cca07eac
AWP22nd <aaawwwppp22@gmail.com>
2026-07-27 19:28:29 +0700

    Update index1.js

--- cuy-university/sesi10/index1.js
+++ cuy-university/sesi10/index1.js
@@ -1,5 +1,27 @@
 // orient class
-
 class Hewan {
     warna
+    makanan
+
+    constructor(nama) {
+        this.nama = nama
+    }
+
+    set newColor(color) {
+        this.warna = color
+    }
+    set newFood(food) {
+        this.makanan = food
+    }
+
+    get detail() {
+        return `Hi! I'm ${this.nama}, ${this.warna} cat, I like eating ${this.makanan}!`
+    }
+
 }
+
+const kucing = new Hewan('bow')
+kucing.newColor = "Blonde"
+kucing.newFood = "Whiskas"
+
+console.log(kucing.detail)
